e2af899b69076a164c96bd5c7a011fa462c2fd6b
Avinash Batchala <a.batchala@vikes.csuohio.edu>
2021-04-17 15:59:45 -0400

    password hashing is implemented using sha 256 algo

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,42 +1,55 @@
-var mongoose = require("mongoose")
+var mongoose = require("mongoose");
 
 var userSchema = new mongoose.Schema({
- name: {
-     type: String,
-     required: true,
-     maxlength: 40,
-     trim: true
- },
- lastname: {
-     type: String,
-     required: false,
-     maxlength: 40,
-     trim: true
- },
- email: {
-     type: String,
-     trim: true,
-     required: true,
-     unique: true
- },
- userinfo: {
-     type: String,
-     trim: truncate
- },
- //ToDo: come back here for password
- password: {
-     type: String,
-     trim: true,
- },
- salt: String,
- role: {
-     type: Number,
-     default: 0
- },
- purchases: {
-     type: Array,
-     default: []
- }
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    maxlength: 32,
+    trim: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true
+  },
+  userinfo: {
+    type: String,
+    trim: true
+  },
+  //TODO: come back here
+  encry_password: {
+    type: String,
+    required: true
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0
+  },
+  purchases: {
+    type: Array,
+    default: []
+  }
 });
 
-module.exports = mongoose.model("User", userSchema )
+userSchema.method = {
+  securePassword: function(plainpassword) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
